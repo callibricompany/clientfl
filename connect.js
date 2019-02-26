c09c3e9297bb26a1ec12e4a13807c33c07cc9da8
bonhommeepargne <bonhomme.epargne@gmail.com>
2019-02-26 21:51:00 +0100

    ajout des variable env

--- connect.js
+++ connect.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
 var firebase = require('firebase');
 
 // Initialize Firebase
 
 var config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID
 };
 firebase.initializeApp(config);
+
+console.log(process.env.APIKEY);
 
 module.exports = firebase;
